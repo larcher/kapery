ad7004024c4c79c4aa2e4f14f2ecbd0e34b42ed9
Larry Archer <larcher@spoon.cx>
2017-06-24 23:53:12 -0500

    whitespace

--- autopan.ino
+++ autopan.ino
@@ -2,6 +2,7 @@
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
 Servo panner;
+
 int angle = 0;
 int min_angle = 0;
 int max_angle = 180;
@@ -13,67 +14,68 @@ bool cw = true;
 bool connectToCloud = true;
 
 void setup() {
-    panner.attach(A5);
-    pinMode(D7, OUTPUT);
-    attachInterrupt(D1, connect, FALLING);
+  panner.attach(A5);
+  pinMode(D7, OUTPUT);
+  attachInterrupt(D1, connect, FALLING);
 
-    Particle.variable("angle", angle);
-    Particle.variable("max_angle", max_angle);
-    Particle.variable("min_angle", min_angle);
-    Particle.variable("inc", angle_inc);
+  Particle.variable("angle", angle);
+  Particle.variable("max_angle", max_angle);
+  Particle.variable("min_angle", min_angle);
+  Particle.variable("inc", angle_inc);
 
-    Particle.function("toggle_pan", togglePanning);
-    Particle.function("max_angle", setMaxAngle);
-    Particle.function("min_angle", setMinAngle);
-    Particle.function("angle", setAngle);
-    Particle.function("inc", setIncrement);
+  Particle.function("toggle_pan", togglePanning);
+  Particle.function("max_angle", setMaxAngle);
+  Particle.function("min_angle", setMinAngle);
+  Particle.function("angle", setAngle);
+  Particle.function("inc", setIncrement);
 }
 
 int togglePanning(String foo) {
   pan_enabled = ! pan_enabled;
 }
+
 int setIncrement(String new_increment) {
-    angle_inc = new_increment.toInt();
+  angle_inc = new_increment.toInt();
 }
+
 int setAngle(String new_angle) {
-    angle = new_angle.toInt();
-    panner.write(angle);
+  angle = new_angle.toInt();
+  panner.write(angle);
 }
 
 int setMaxAngle(String new_max_angle) {
-    max_angle = new_max_angle.toInt();
+  max_angle = new_max_angle.toInt();
 }
 
 int setMinAngle(String new_min_angle) {
-    min_angle = new_min_angle.toInt();
+  min_angle = new_min_angle.toInt();
 }
 
 void loop() {
-    if(connectToCloud && Particle.connected() == false) {
-        Particle.connect();
-        //connectToCloud = false;
-    }
+  if(connectToCloud && Particle.connected() == false) {
+    Particle.connect();
+    //connectToCloud = false;
+  }
 
   if (pan_enabled) {
-  if (cw) { 
+    if (cw) {
       angle += angle_inc;
-  } else {
+    } else {
       angle -= angle_inc;
-  }
-  if (angle >= max_angle) {
+    }
+    if (angle >= max_angle) {
       angle = max_angle;
       cw = false;
-  }
-  if (angle <= min_angle) {
+    }
+    if (angle <= min_angle) {
       angle = min_angle;
       cw = true;
+    }
+    panner.write(angle);
+    delay(interval);
   }
-  panner.write(angle);
-  delay(interval);
-  }
-
 }
 
 void connect() {
-    connectToCloud = true;
+  connectToCloud = true;
 }
