a67458468e39e670c1df6d1303f96f8896905f49
Larry Archer <larcher@users.noreply.github.com>
2017-06-24 23:35:24 -0500

    Adding back the Particle.connect ( on by default for now)
    connect() is not called automatically when in SEMI_AUTOMATIC mode.

--- autopan.ino
+++ autopan.ino
@@ -1,30 +1,49 @@
+
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
 Servo panner;
 int angle = 0;
-int interval=500;
-bool cw = true;
-int max_angle = 180;
 int min_angle = 0;
+int max_angle = 180;
+int angle_inc = 15;
+int interval=500;
+bool pan_enabled = false;
+bool cw = true;
+
+bool connectToCloud = true;
 
 void setup() {
-  panner.attach(A5);
+    panner.attach(A5);
+    pinMode(D7, OUTPUT);
+    attachInterrupt(D1, connect, FALLING);
 }
 
 void loop() {
+    if(connectToCloud && Particle.connected() == false) {
+        Particle.connect();
+        //connectToCloud = false;
+    }
+  
+  if (pan_enabled) {
   if (cw) { 
       angle += angle_inc;
-      if (angle > max_angle) {
+      if (angle >= max_angle) {
           angle = max_angle;
           cw = false;
       }
   } else {
       angle -= angle_inc;
-      if (angle < min_angle) {
+      if (angle <= min_angle) {
           angle = min_angle;
           cw = true;
       }
-  }  
+  }
   panner.write(angle);
   delay(interval);
+  }
+
+}
+
+void connect() {
+    connectToCloud = true;
 }
